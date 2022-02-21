ea15aba6f517780da830e09ed200b6953f1b3019
rrcaddick <rrcaddick@gmail.com>
2022-02-21 17:54:53 +0200

    Updated API Key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const API_KEY = 'a1d50d19-f252-4165-a794-b4ef6a8d1834';
+export const API_KEY = '49284e6c-1807-48ae-b831-c40056fb06b9';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const MODEL_CLOSE_SEC = 2.5;
